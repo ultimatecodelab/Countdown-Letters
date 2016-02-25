c58c521e4e69b8613a6753741cc66bf586f63bda
Ultimatecodelab <adgn353@gmail.com>
2016-02-25 16:47:29 +0000

    solver function added, which returns the anagrams of the specified word

--- CountdownV1.py
+++ CountdownV1.py
@@ -3,6 +3,8 @@
 #Arjun Kharel - Software Development (GMIT)
 
 wordmap = dict()
+result = dict()
+MIN_WORD_LENGTH = 4
 
 def preprocessing():
 	wordsList = [line.rstrip('\n') for line in open('words.txt')]
@@ -19,9 +21,21 @@ def addToMap(key,value):
 	else:
 		wordmap.update({key:[value]})
 
+def solver(word):
+	convertedKey = sum(bytearray(word,'utf8'))
+	if(len(word)>=MIN_WORD_LENGTH):
+		countdownLetters = wordmap.get(convertedKey)
+		if(convertedKey in wordmap):
+			for str in countdownLetters:
+				if sorted(word)==sorted(str) and str not in result:
+					result.update({str:len(str)})
+					
+	return result
+	
 def beginProcess():
 	#end of preprocessing
 	preprocessing()
 	print(len(wordmap))
+	print(solver("education"))
 	
 beginProcess()
