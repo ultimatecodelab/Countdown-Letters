8880faf7415deca20b8f0a93a7b4ee6cffc8c281
Ultimatecodelab <adgn353@gmail.com>
2016-02-28 15:39:27 +0000

    function added to generate random word based on alphabets frequency

--- CountdownV1.py
+++ CountdownV1.py
@@ -4,6 +4,8 @@
 
 import pickle
 import operator
+from random import sample, randint
+
 result = dict()
 MIN_WORD_LENGTH = 6
 pickle_in = open("dict.pickle","rb")
@@ -11,8 +13,8 @@ wordmap = pickle.load(pickle_in)
 tempList = list()
 maxLength = 0
 
+
 def solver(word):
-	#trackerCount = len(word
 	convertedKey = sum(bytearray(word,'utf8'))
 	if(len(word)>=MIN_WORD_LENGTH):
 		countdownLetters = wordmap.get(convertedKey)
@@ -20,24 +22,36 @@ def solver(word):
 			for str in countdownLetters:
 				if sorted(word)==sorted(str) and str not in result:
 					result.update({str:len(str)})
-		
-		##if(len(word)==9 and len(result)>0):
-			##return result
 		tempList = list(word)
-		for i in range(len(tempList)): #eoibklhlr software
+		for i in range(len(tempList)):
 			charTmp = tempList.pop()
 			wordStr =''.join(tempList)
 			tempList.insert(0,charTmp)
-			solver(wordStr)
+			solver(wordStr)##
 	return result
+	
+def generateWord():
+	VOWELS = 'eeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaiiiiiiiiiiiiiooooooooooooouuuuu'
+	CONSONANTS = 'bbcccddddddffggghhjklllllmmmmnnnnnnnnppppqrrrrrrrrrssssssssstttttttttvwxyz'
+	n_vowels = randint(3,4)
+	vowels = sample(VOWELS, n_vowels)
+	consonants = sample(CONSONANTS, 9 - n_vowels)
+	letters = vowels+consonants
+	print(letters)
+	word = ''.join(letters)
+	#print(word)
+	return word
 	
 def beginProcess():
 	#end of preprocessing
 	#preprocessing()
-	countdownLetters = solver("education")
+	word = generateWord()
+	countdownLetters = solver(word)
 	if(countdownLetters):
 		#sort the words from highest length to lowest (big to small)
 		sorted_x = sorted(result.items(), key=operator.itemgetter(1), reverse = True)
-	print(countdownLetters)
+		print(sorted_x)
 	
 beginProcess()
+##References
+#http://www.thecountdownpage.com/letters.htm letter frequencies
