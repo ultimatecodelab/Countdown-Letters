e5f70125daf1e8ff1b66bcd1980bf65d82b1630e
Ultimatecodelab <adgn353@gmail.com>
2016-02-29 20:59:52 +0000

    Optimised the running time

--- CountdownV1.py
+++ CountdownV1.py
@@ -5,31 +5,33 @@
 #Arjun Kharel - Software Development (GMIT)
 
 import pickle
+import timeit
 import operator
 from random import sample, randint
-from collections import deque
+from collections import deque ##You can add and remove items on both ends of a deque at O(1) cost.
 
 result = dict()
-MIN_WORD_LENGTH = 6
+MIN_WORD_LENGTH = 6 #the depth you want to do down..
 pickle_in = open("dict.pickle","rb")
 wordmap = pickle.load(pickle_in)
-##tempList = list()
 maxLength = 0
 
-def solver(word):
-	convertedKey = sum(bytearray(word,'utf8'))
+def solver(word,iteration):
+	sortedWord = sorted(word)
+	key = ''.join(sortedWord)
 	if(len(word)>=MIN_WORD_LENGTH):
-		countdownLetters = wordmap.get(convertedKey)
-		if(convertedKey in wordmap):
+		countdownLetters = wordmap.get(key)
+		if(key in wordmap):
 			for str in countdownLetters:
-				if sorted(word)==sorted(str) and str not in result:
+				if sortedWord==sorted(str) and str not in result:
 					result.update({str:len(str)})
+		
 		tempList = deque(word)
 		for i in range(len(tempList)):
 			charTmp = tempList.pop()
 			wordStr =''.join(tempList)
 			tempList.insert(0,charTmp)
-			solver(wordStr)##recursively calling the solver function
+			solver(wordStr,(i+1))##recursively calling the solver function
 	return result
 	
 def generateWord():
@@ -48,26 +50,28 @@ def generateWord():
 	vowels = sample(VOWELS, n_vowels)
 	consonants = sample(CONSONANTS, 9 - n_vowels)
 	letters = vowels+consonants
-	print(letters)
+	##print(letters)
 	word = ''.join(letters)
-	#print(word)
+	print(word)
 	return word
 	
 def beginProcess():
-	#end of preprocessing
-	#preprocessing()
-	##word = generateWord()
 	word = generateWord()
-	countdownLetters = solver(word)
+	#word = "cnthello"
+	countdownLetters = solver(word,1)
 	if(countdownLetters):
 		#sort the words from highest length to lowest (big to small)
 		sorted_x = sorted(result.items(), key=operator.itemgetter(1), reverse = True)
 		print(sorted_x)
 	
 beginProcess()
+##from countdownV1 import beginProcess
+##print(timeit.timeit('beginProcess()', setup='from countdownV1 import beginProcess', number=100))
+
 ##References
 #http://www.thecountdownpage.com/letters.htm letter frequencies
 ##https://docs.python.org/2/library/random.html#random.sample
 ##https://docs.python.org/2/library/operator.html
 ##http://stackoverflow.com/questions/3277503/python-read-file-line-by-line-into-array
 ##http://stackoverflow.com/questions/8452961/convert-string-to-ascii-value-python
+##http://stackoverflow.com/questions/280243/python-linked-list
--- picklerV1.py
+++ picklerV1.py
@@ -5,12 +5,14 @@ def preprocessing():
 	wordsList = [line.rstrip('\n') for line in open('words.txt')]
 	for word in wordsList:
 		#generate the key #generate the value
-		key = sum(bytearray(word,'utf8')) #sum of the word, ascii value
-		addToMap(key,word)
+		#key = sum(bytearray(word,'utf8')) #sum of the word, ascii value
+		key = sorted(word)
+		sortedKey = ''.join(key)
+		addToMap(sortedKey,word)
 
 #adding ascii value as a key and list of words as values
 def addToMap(key,value):
-	if(len(value)>=4 and len(value)<=9):
+	if(len(key)>=4 and len(key)<=9):
 		if key in wordmap:
 			wordmap.get(key).append(value) #if key exists, get the reference to the list(value) and add it.
 		else:
