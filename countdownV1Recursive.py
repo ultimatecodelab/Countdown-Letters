889a3d4d140451603200139d7eabd61b1e9c1f3f
Ultimatecodelab <adgn353@gmail.com>
2016-03-13 18:14:30 +0000

    menu added on countdownV1Recursive version, code commented

--- countdownV1Recursive.py
+++ countdownV1Recursive.py
@@ -1,51 +1,77 @@
-#countdown letters implementation using recursion 
-#Countdown Solver without generating permutations
-#Allows user to enter the min word(length), they would like to extract
-#from the original word.
-#Arjun Kharel - Software Development (GMIT)
+#Arjun Kharel - Software Development (GMIT) - Theory  of Algorithms
+#countdown letters implementation using recursion - We are NOT generating permutations of a word
 
 import pickle
 import timeit
 import operator
-from conundrumGenerator import generateWord
+from conundrumGenerator import generateWord #importing generateWord() function from  conundrumGenerator.py 
 from random import sample, randint
 from collections import deque ##You can add and remove items on both ends of a deque at O(1) cost.
 
-result = dict()
-MIN_WORD_LENGTH = 6 #the depth you want to do down..
-pickle_in = open("dict.pickle","rb")
-wordmap = pickle.load(pickle_in)
-maxLength = 0
+result = dict()#dict to hold the countdown letters. key will be the word itself and value will be the length of the words.
+MIN_WORD_LENGTH = 6 #the depth you want to do down in the recusion..
+TERMINATE = 0
+GENERATE_CONUNDRUM = 1
 
-def solver(word,iteration):
+pickle_in = open("dict.pickle","rb") 
+wordmap = pickle.load(pickle_in)#loading the(pickeled) dictionary
+
+def solver(word):
 	sortedWord = sorted(word)
 	key = ''.join(sortedWord)
 	if(len(word)>=MIN_WORD_LENGTH):
 		countdownLetters = wordmap.get(key)
 		if(key in wordmap):
 			for str in countdownLetters:
-				if sortedWord==sorted(str) and str not in result:
+				if sortedWord==sorted(str) and str not in result: #checking sortedWord is same as the words in resultset
 					result.update({str:len(str)})
-		
-		tempList = deque(word)
+					
+		tempList = deque(word)#This is exactly I needed for this particular problem. I wanted to take the element from tail and append to
+		#the head. This can be done using deque data structure at O(1) cost.
 		for i in range(len(tempList)):
-			charTmp = tempList.pop()
+			charTmp = tempList.pop()#popping from the end of the deque
 			wordStr =''.join(tempList)
-			tempList.insert(0,charTmp)
-			solver(wordStr,(i+1))##recursively calling the solver function
+			tempList.insert(0,charTmp)#inserting to head
+			solver(wordStr)##recursively calling the solver function
 	return result
+
+def optionsMenu(): #this displays the menu and when valid option is selected it calls beginProcess()
+	mode=GENERATE_CONUNDRUM; #flag
+	while(mode != TERMINATE):
+		try:
+			mode=int(input('Enter 1 to generate the random conundrum or 0 to exit:'))
+			if(mode==1):
+				beginProcess()
+			else:
+				print("GoodBye...")
+		except ValueError:
+			print ("Enter 1 to generate the random conundrum or 0 to exit:")
 	
 def beginProcess():
-	word = generateWord()
-	countdownLetters = solver(word,1)
+	word = generateWord() #generates random word
+	countdownLetters = solver(word)
 	if(countdownLetters):
-		#sort the words from highest length to lowest (big to small)
-		sorted_x = sorted(result.items(), key=operator.itemgetter(1), reverse = True)
+		print("\nBelow is the result of the longest anagrams of random conundrum: ",word)
+		
+		#sorted_x will be a list of tuples sorted by the second element(value) in each tuple.
+		sorted_x = sorted(result.items(), key=operator.itemgetter(1), reverse = True) #
+		
+		#sorted_x= sorted(result.items(), key=lambda x: x[1], reverse=True) 
+		#could use lambda key=lambda x: x[1] to sort as well. Tested with both ways but THERE is not much difference at all.
+		
+		print("Total words found: " , len(sorted_x),' with recursion depth (min word length) of : ',MIN_WORD_LENGTH,'\n')
 		print(sorted_x)
-	
-beginProcess()
-##from countdownV1 import beginProcess
-##print(timeit.timeit('beginProcess()', setup='from countdownV1 import beginProcess', number=100))
+		print('------------------------------------------------------------------------------------------------------')
+		result.clear()
+	else:
+		print("Ooops, couldn't find the countdown letters/anagrams for the word: " ,word)
+
+optionsMenu() #program starts from here...
+
+#beginProcess()
+#uncomment the print statements if you would like to test the time 
+#from countdownV1Recursive import beginProcess
+#print(timeit.timeit('beginProcess()', setup='from countdownV1Recursive import beginProcess', number=100))
 
 ##References
 #http://www.thecountdownpage.com/letters.htm letter frequencies
@@ -54,3 +80,4 @@ beginProcess()
 ##http://stackoverflow.com/questions/3277503/python-read-file-line-by-line-into-array
 ##http://stackoverflow.com/questions/8452961/convert-string-to-ascii-value-python
 ##http://stackoverflow.com/questions/280243/python-linked-list
+##http://stackoverflow.com/questions/4690416/sorting-dictionary-using-operator-itemgetter
--- countdownV2Perm.py
+++ countdownV2Perm.py
@@ -1,3 +1,9 @@
+#Arjun Kharel - Software Development (GMIT) 
+#Countdown Solver by generating permutations
+#Significantly faster than countdownV1Recursive
+#please read through the github readme. I have compare and constrasted
+#both algorithms.
+
 import pickle
 from conundrumGenerator import generateWord
 from itertools import combinations
@@ -11,16 +17,21 @@ TERMINATE = 0
 GENERATE_CONUNDRUM = 1
 
 def solver(letters):
-	letters = sorted(letters)
-	for i in reversed(range(MIN_RANGE,MAX_RANGE)):
-		result = set()
+	letters = sorted(letters) ##sorting alphabets
+	for i in reversed(range(MIN_RANGE,MAX_RANGE)): #reverse means we are starting from high to low value. Decending order.
+		result = set() #set to hold the result (countdown letters)
 		for permutations in combinations(letters,i): #itertools.combinations()
+			#we are looping through the list returned by the combinations function from itertools. 
+			#"Return r length subsequences of elements from the input iterable."
+			
 			sortedWord = ''.join(permutations)
 			if(sortedWord in wordmap):
 				for word in wordmap[sortedWord]:
 					result.add(word)
 		if(result):
 			return result #we fond the longest
+		#if you want to get all the words from length max (9) to min(4) than
+		#comment if(result) then you can see all the words in decending order.
 	return result
 	
 ##This function mycombinations(k,available,used) can also be used for  generating permutations. This is slower than
@@ -60,7 +71,7 @@ def solver2(letters):
 	return result
 '''
 def optionsMenu():
-	mode=GENERATE_CONUNDRUM;
+	mode=GENERATE_CONUNDRUM; #flag
 	while(mode != TERMINATE):
 		try:
 			mode=int(input('Enter 1 to generate the random conundrum or 0 to exit:'))
@@ -70,10 +81,11 @@ def optionsMenu():
 				print("GoodBye...")
 		except ValueError:
 			print ("Enter 1 to generate the random conundrum or 0 to exit:")
+
 ##TEST Word : ioeecdrln
 def beginProcess():
-	word = "ioeecdrln"
-	#word = generateWord()#this function is implemented in conundrumGenerator.py. We are reusing the function.
+	##word = "ioeecdrln"
+	word = generateWord()#this function is implemented in conundrumGenerator.py. We are reusing the function.
 	print("\nBelow is the result of the longest anagrams of random conundrum: ",word)
 	tempResult = solver(word)
 	#tempResult2 = solver2(word) ## UNCOMMENT the myCombinations() and solver2() and only uncomment this line
@@ -81,8 +93,15 @@ def beginProcess():
 	print(tempResult)
 	
 optionsMenu()
-	
+
+## test for time - please uncomment the below code and comment all the print statements
 ##from countdownV2Perm import beginProcess
 ##print(timeit.timeit('beginProcess()', setup='from countdownV2Perm import beginProcess', number=1000))
 
-##[01]https://docs.python.org/2/library/itertools.html#itertools.combinations
+##https://docs.python.org/2/library/itertools.html#itertools.combinations
+#http://www.thecountdownpage.com/letters.htm letter frequencies
+##https://docs.python.org/2/library/random.html#random.sample
+##https://docs.python.org/2/library/operator.html
+##http://stackoverflow.com/questions/3277503/python-read-file-line-by-line-into-array
+##http://stackoverflow.com/questions/8452961/convert-string-to-ascii-value-python
+##http://stackoverflow.com/questions/280243/python-linked-list
